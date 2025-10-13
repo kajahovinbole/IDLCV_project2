1ef5b04f172778e4a0943bd9bd274102773d3d21
Kaja Hovinbøle <kajahovin@icloud.com>
2025-10-13 12:23:06 +0200

    added datasets from learn

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ NUM_CLASSES = 10
 TRAINABLE_BLOCKS = 0
 FREEZE_BACKBONE = True
 NUM_EPOCHS = 1
+
+DATA_ROOT = "placeholder_for_data_root"  # sett riktig sti i config.py
+N_FRAMES = 10
+BATCH_SIZE = 64
+NUM_WORKERS = 8
