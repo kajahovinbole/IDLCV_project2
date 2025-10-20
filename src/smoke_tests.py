86b67a05df3ac04fe9330a7aafba98d1b92d7ca3
Kaja Hovinbøle <kajahovin@gmail.com>
2025-10-20 10:43:09 +0200

    blæ

--- src/smoke_tests.py
+++ src/smoke_tests.py
@@ -1,8 +1,11 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 import torch
 from src.data.datasets import FrameVideoDataset
 from src.models.models import SingleFrameModel
 from src.utils.video_utils import logits_mean_over_time
-from src.utils.transforms import get_single_frame_transform
+from utils.transforms import get_single_frame_transform
 from config import DATA_ROOT, NUM_CLASSES, N_FRAMES
 
 def main():
